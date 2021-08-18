a6d1073e624cb0d50b7df30a9049401e72924425
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-08-18 13:50:59 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 🔧 V{BOT_VER} [Has Been Activated!]")
+    f"RIO-𝐔SERBOT 🔧 V{BOT_VER} [Has Been Activated!]")
 
 
 if len(argv) not in (1, 3, 4):
