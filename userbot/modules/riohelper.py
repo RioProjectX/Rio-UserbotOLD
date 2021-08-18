5199c95a5c4a4ad7f1585846d5f67b23891c58e1
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-08-18 08:15:16 +0700

    Update and rename kenhelper.py to riohelper.py

--- userbot/modules/riohelper.py
+++ userbot/modules/riohelper.py
@@ -12,22 +12,22 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/xgothboi)"
-        "\n[Repo](https://github.com/KennedyProject/KEN-UBOT)"
-        "\n[Instagram](instagram.com/acxken._)")
+        "\n[Telegram](t.me/riio00)"
+        "\n[Repo](https://github.com/RioProjectX/Rio-Userbot)"
+        "\n[Group Support](https://t.me/siiniaja)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/KennedyProject/KEN-UBOT/KEN-UBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/RioProjectX/Rio-Userbot/Rio-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
     "`.ghelp`\
-\nUsage: Bantuan Untuk KEN-UBOT.\
+\nUsage: Bantuan Untuk ★RIO-USERBOT★.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
