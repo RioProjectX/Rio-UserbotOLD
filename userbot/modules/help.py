53b29f7809c080f498d5b73fa426f62d065c1318
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-08-19 10:16:00 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t|  "
-        await event.edit("**⚡ 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 ⚡**\n\n"
+        await event.edit("**⚡ RIO-𝐔SERB𝐎𝐓 ⚡**\n\n"
                          f"**❒ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**❒ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**❒ Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉| {string}◉\n\n")
