22d2bf47bec2c3cb5c59e11429e04a910ed7e79a
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-09-05 19:44:31 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -112,7 +112,7 @@ async def repo_is_here(wannasee):
     await wannasee.edit(
         "**Hey**, I am using **RIO-𝐔𝐁𝐎𝐓** \n"
         "➣ **Repo :** [Github](https://github.com/RioProjectX/Rio-Userbot)\n"
-        "➣ **Owner :** [Rii](t.me/riio00)"
+        "➣ **Owner :** [Rio](t.me/riio00)"
     )
 
 
