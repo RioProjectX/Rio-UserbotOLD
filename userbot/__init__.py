17e57404a041ee2317fbdf3d6476ee18166ba1e0
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-11-10 14:06:02 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -109,9 +109,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/RioProjectX/Rio-Userbot")
+    "https://github.com/RioProjectX/Rio-UserbotNEW")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Rio-Userbot")
+    "UPSTREAM_REPO_BRANCH", "Rio-UserbotNEW")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
